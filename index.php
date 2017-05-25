24d2f62fd1bce8a837e3e9b2a5d7cd9d3310b23d
karthiraja888 <karthiraja888@gmail.com>
2017-05-25 15:30:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head> <link rel="stylesheet" href="home.css"></head>
+<body >
+   <form action="eyes.php">
+    
+    <h1>Eyes-Book</h1>
+       
+    <div class="container margindiv">
+    <label class="spacelab">UserName/ E-mail ID</label>
+    <input class="space" type="email" name="email" placeholder="UserName" ><br>
+    <label class="spacelab">Password</label>
+    <input class="space" type="password" name="password" placeholder="Password" ><br>
+    <input class="btn margin" type="button" name="Login" value="Login">
+    <input class="btn" type="button" value="Sign Up">
+        
+        </div>
+</form>
+    </body>
+</html>
